b25a37885ac7cf2ff8fd2cc24b80a2d4bfc4cba6
Zen Wong <zh3nhua94@gmail.com>
2023-09-21 10:29:14 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+import AddItem from './AddItem';
+import SearchItem from './SearchItem';
 
 function App() {
+  const [items, setItems] = useState(() => {
+    if(localStorage.getItem('shoppinglist') == null) {
+      console.log('no shopping list yet');
+      return '';
+    }
+    return JSON.parse(localStorage.getItem('shoppinglist'));
+  });
+  const [newItem, setNewItem] = useState('');
+  const [search, setSearch] = useState('');
+
+  const setAndSaveItems = (saveItem) => {
+    setItems(saveItem);
+    localStorage.setItem('shoppinglist', JSON.stringify(saveItem));
+  }
+
+  const handleCheck = (id) => {
+      const listItems = items.map((item) => {
+          return item.id === id ? {...item, checked: !item.checked} : item;
+      });
+      setAndSaveItems(listItems);
+  }
+
+  const handleDelete = (id) => {
+      const listItems = items.filter((item) => {
+          return item.id !== id;
+      })
+      setAndSaveItems(listItems);
+  }
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = {
+      id,
+      checked: false,
+      item
+    }
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!newItem) return;
+    addItem(newItem);
+    setNewItem('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="Grocery List" />
+      <AddItem 
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem
+        search={search}
+        setSearch={setSearch}
+      />
+      <Content 
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer length={items.length} />
     </div>
   );
 }
